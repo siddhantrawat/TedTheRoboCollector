b7cb45af5e2a6f33726140f45e32dee5508831c9
siddhantrawat <siddd.cloud@gmail.com>
2019-05-09 23:43:37 +0530

    first commit

---
+++ Assets/scripts/Collector.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A collecting game object
+/// </summary>
+public class Collector : MonoBehaviour
+{
+    #region Fields
+
+    // targeting support
+    SortedList<Target> targets = new SortedList<Target>();
+    Target targetPickup = null;
+    
+    // movement support
+    const float BaseImpulseForceMagnitude = 2.0f;
+    const float ImpulseForceIncrement = 0.3f;
+
+    // saved for efficiency
+    Rigidbody2D rb2d;
+    string a;
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        // center collector in screen
+        Vector3 position = transform.position;
+        position.x = 0;
+        position.y = 0;
+        position.z = 0;
+        transform.position = position;
+
+        // save reference for efficiency
+        rb2d = GetComponent<Rigidbody2D>();
+
+        // add as listener for pickup spawned event
+        EventManager.AddListener(AddToSortedList);
+    }
+
+    /// <summary>
+    /// Called when another object is within a trigger collider
+    /// attached to this object
+    /// </summary>
+    /// <param name="other"></param
+    /// >
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        // only respond if the collision is with the target pickup
+        if (other.gameObject == targetPickup.GameObject)
+        {
+
+            // remove collected pickup from list of targets and game
+            targets.IndexOf(targetPickup);
+            Debug.Log("destroy index : " + targets.IndexOf (targetPickup));
+            targets.RemoveAt(targets.IndexOf(targetPickup));
+
+            Destroy(targetPickup.GameObject);
+            
+            if (targets.Count != 0)
+            {
+                for (int i = 0; i < targets.Count; i++)
+                {
+                    targets[i].UpdateDistance(transform.position);
+                }
+
+                //support code 
+                /*
+                a = a + "unsorted list    ";
+                for (int i = 0; i < targets.Count; i++)
+                    a = a + "," + targets[i].Distance;
+                Debug.Log(a + "and target pickup equals " + targetPickup.Distance);
+                a = " ";
+                */
+                targets.Sort();
+                /*
+                a = a + "sorted list    ";
+                for (int i = 0; i < targets.Count; i++)
+                    a = a + "," + targets[i].Distance;
+                Debug.Log(a + "and target pickup equals " + targetPickup.Distance);
+                a = " ";
+                */
+
+                SetTarget(targets[targets.Count - 1].GameObject);
+                //Debug.Log("new target set");
+            }
+            else           
+              SetTarget(null);
+
+
+        }
+    }
+    
+    void AddToSortedList(GameObject pickup)
+    {
+
+        Target target = new Target(pickup.gameObject, gameObject.transform.position);
+        targets.Add(target);
+        
+        
+        if (targetPickup != null)
+        {
+            if (target.Distance < targetPickup.Distance)
+            {
+                //Debug.Log("new closest target");
+                SetTarget(pickup);
+            }
+        }
+        else
+        {
+             SetTarget(pickup);
+
+        }
+        /*
+        for (int i = 0; i < targets.Count; i++)
+            a = a + "," + targets[i].Distance;
+        Debug.Log(a + "and target pickup equals " + targetPickup.Distance);
+        a = " ";
+        */
+
+
+    }
+
+    /// <summary>
+    /// Sets the target pickup to the provided pickup
+    /// </summary>
+    /// <param name="pickup">Pickup.</param>
+    void SetTarget(GameObject pickup)
+    {
+        targetPickup = new Target (pickup.gameObject, gameObject.transform.position);
+        GoToTargetPickup();
+    }
+
+    /// <summary>
+    /// Starts the teddy bear moving toward the target pickup
+    /// </summary>
+    
+      void GoToTargetPickup()
+    {
+        Debug.Log("moving toward " + targets.IndexOf(targetPickup));
+        // calculate direction to target pickup and start moving toward it
+		Vector2 direction = new Vector2(
+			targetPickup.GameObject.transform.position.x - transform.position.x,
+			targetPickup.GameObject.transform.position.y - transform.position.y);
+		direction.Normalize();
+		rb2d.velocity = Vector2.zero;
+        
+		rb2d.AddForce(direction * (BaseImpulseForceMagnitude + ImpulseForceIncrement * GameObject.FindGameObjectsWithTag("Pickup").Length),
+			ForceMode2D.Impulse);
+	}
+	
+
+#endregion
+}
---
+++ Assets/scripts/SortedList.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A sorted list
+/// </summary>
+public class SortedList<T> where T:IComparable
+{
+    List<T> items = new List<T>();
+
+    // used in Add method
+    List<T> tempList = new List<T>();
+	
+    #region Constructors
+
+    /// <summary>
+    /// No argument constructor
+    /// </summary>
+    public SortedList()
+    {
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the number of items in the list
+    /// </summary>
+    /// <value>number of items in the list</value>
+    public int Count
+    {
+        get { return items.Count; }
+    }
+	
+    /// <summary>
+    /// Gets the item in the array at the given index
+    /// This property allows access using [ and ]
+    /// </summary>
+    /// <param name="index">index of item</param>
+    /// <returns>item at the given index</returns>
+    public T this[int index]
+    {
+        get { return items[index]; }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Adds the given item to the list
+    /// </summary>
+    /// <param name="item">item</param>
+    public void Add(T item)
+    {
+        
+        // add your implementation below
+        int locationToAdd = 0;
+
+        tempList.Add(item);
+       
+        for ( locationToAdd = 0; locationToAdd < items.Count && item.CompareTo(items[locationToAdd]) < 0; locationToAdd++) ;
+
+
+        if (locationToAdd == Count)
+        {
+            items.Add(item);
+        }
+        else
+        {
+            items.InsertRange(locationToAdd, tempList);
+        }
+        tempList.Remove(item);
+
+    }
+
+    /// <summary>
+    /// Removes the item at the given index from the list
+    /// </summary>
+    /// <param name="index">index</param>
+    public void RemoveAt(int index)
+    {
+        items.RemoveAt(index);
+        Debug.Log("destoryed at index : "+index);
+        // add your implementation below
+    }
+
+    /// <summary>
+    /// Determines the index of the given item using binary search
+    /// </summary>
+    /// <param name="item">the item to find</param>
+    /// <returns>the index of the item or -1 if it's not found</returns>
+    public int IndexOf(T item)
+    {
+        int lowerBound = 0;
+        int upperBound = items.Count - 1;
+        int location = -1;
+
+        // loop until found value or exhausted array
+        while ((location == -1) &&
+            (lowerBound <= upperBound))
+        {
+            // find the middle
+            int middleLocation = lowerBound + (upperBound - lowerBound) / 2;
+            T middleValue = items[middleLocation];
+
+            // check for match
+            if (middleValue.CompareTo(item) == 0)
+            {
+                location = middleLocation;
+         //       Debug.Log(location);
+            }
+            else
+            {
+                // split data set to search appropriate side
+                if (middleValue.CompareTo(item) > 0)
+                {
+                    
+           //         Debug.Log("greater than middle");
+                    if (lowerBound != items.Count - 1)
+                        lowerBound = middleLocation + 1;
+                    else
+                        lowerBound = middleLocation;
+                }
+                else
+                {
+             //       Debug.Log("less than middle");
+                    upperBound = middleLocation - 1;
+                }
+            }
+        }
+        //Debug.Log("index to destroy : "+location);
+        return location;
+    }
+
+    /// <summary>
+    /// Sorts the list
+    /// </summary>
+    public void Sort()
+    {
+        SortedList<T> temp = new SortedList<T>();
+        for (int i = 0; i < items.Count; i++)
+            temp.Add(items[i]);
+
+        for (int i = 0; i < items.Count; i++)
+            items[i] = temp[i];
+    }
+
+    #endregion
+}
